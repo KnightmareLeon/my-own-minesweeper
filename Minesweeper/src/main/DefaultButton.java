511b4f151caf0432b62832d0a9e6e6948846256c
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-03-13 19:20:07 +0800

    Fixed buttons getting clicked when SPACEBAR is pressed

--- Minesweeper/src/main/DefaultButton.java
+++ Minesweeper/src/main/DefaultButton.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.KeyStroke;
 
 public abstract class DefaultButton extends JButton{
     private final Color DEFAULT_COLOR = new Color(209, 208, 206);
@@ -12,6 +13,7 @@ public abstract class DefaultButton extends JButton{
     public DefaultButton(){
         this.setBackground(DEFAULT_COLOR);
         this.setFocusPainted(false);
+        this.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "none");
         this.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseEntered(MouseEvent e){
