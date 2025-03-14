128811ebe0069bde20b34a654aa53a649c416495
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-03-14 11:35:00 +0800

    Set Frame Icon and some optimizations

--- Minesweeper/src/main/MainFrame.java
+++ Minesweeper/src/main/MainFrame.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -19,6 +20,7 @@ public class MainFrame extends JFrame{
     
     private Toolkit toolkit = Toolkit.getDefaultToolkit();
     private final int WINDOW_SIZE = toolkit.getScreenSize().height - 50;
+    private final Image WINDOW_ICON = toolkit.getImage("src/resources/images/Mine.png");
 
     public static final byte MAIN_MENU = 0;
     public static final byte START_NEW_GAME_MENU = 1;
@@ -29,9 +31,11 @@ public class MainFrame extends JFrame{
         this.setSize(WINDOW_SIZE, WINDOW_SIZE);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
+        this.setIconImage(WINDOW_ICON);
         this.setTitle("Minesweeper");
 
         this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setContentPane(MAIN_MENU);
         this.setVisible(true);
         this.addWindowListener(new WindowAdapter() {
             @Override
